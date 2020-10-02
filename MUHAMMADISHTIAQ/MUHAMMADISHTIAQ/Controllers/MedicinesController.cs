3d332d476b30ff742fb54e1ec2df7c375407215e
Muhammad Ishtiaq <71250626+MUHAMMAD343@users.noreply.github.com>
2020-10-02 18:43:53 +0500

    First Commit

---
+++ MUHAMMADISHTIAQ/MUHAMMADISHTIAQ/Controllers/MedicinesController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MUHAMMADISHTIAQ.Models;
+
+namespace MUHAMMADISHTIAQ.Controllers
+{
+    public class MedicinesController : Controller
+    {
+        private MedcineDBContext db = new MedcineDBContext();
+
+        // GET: Medicines
+        public ActionResult Index(string SearchBy, string Search)
+        {
+            if (SearchBy == "Name")
+            {
+                var data = db.Medicines.Where(x => x.Name == Search).ToList();
+                return View(data);
+            }
+            else if (SearchBy == "Category")
+            {
+                var data = db.Medicines.Where(x => x.Category == Search).ToList();
+                return View(data);
+            }
+            else
+            {
+                var data = db.Medicines.ToList();
+                return View(data);
+            }
+
+        }
+
+
+        // GET: Medicines/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicine medicine = db.Medicines.Find(id);
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicine);
+        }
+
+        // GET: Medicines/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Medicines/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Category,StoreBox,PurchasePrice,SellingPrice,Quantity,CompanyName,ModifiedDate,ExpireDate")] Medicine medicine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Medicines.Add(medicine);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(medicine);
+        }
+
+        // GET: Medicines/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicine medicine = db.Medicines.Find(id);
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicine);
+        }
+
+        // POST: Medicines/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Category,StoreBox,PurchasePrice,SellingPrice,Quantity,CompanyName,ModifiedDate,ExpireDate")] Medicine medicine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medicine).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(medicine);
+        }
+
+        // GET: Medicines/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicine medicine = db.Medicines.Find(id);
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicine);
+        }
+
+        // POST: Medicines/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Medicine medicine = db.Medicines.Find(id);
+            db.Medicines.Remove(medicine);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MUHAMMADISHTIAQ/MUHAMMADISHTIAQ/Models/Medicine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MUHAMMADISHTIAQ.Models
+{
+    public class Medicine
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string StoreBox { get; set; }
+        public int PurchasePrice { get; set; }
+        public int SellingPrice{ get; set; }
+        public int Quantity { get; set; }
+        public string CompanyName { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+    public class MedcineDBContext : DbContext
+    {
+        public DbSet<Medicine> Medicines { get; set; }
+    }
+}
